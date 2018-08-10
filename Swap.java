261ccc23a6d942720267094419ed1bafcea187ad
Prathap01 <prathapalur@gmail.com>
2018-08-10 11:40:28 +0530

    Java programs committed

---
+++ Swap.java
@@ -0,0 +1,18 @@
+public class Swap {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		System.out.println("before swaping"+a);
+		System.out.println("before swapping"+b);
+		int temp;
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println("after swaping"+a);
+		System.out.println("after swapping"+b);
+
+	}
+
+}
